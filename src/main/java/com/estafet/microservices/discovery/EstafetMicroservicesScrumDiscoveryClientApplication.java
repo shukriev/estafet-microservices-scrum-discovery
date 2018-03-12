0779b31502764c70170731abc49cd575af9441ba
shukriev <shukri.izetov.shukriev@gmail.com>
2018-03-12 11:27:52 +0200

    initial

---
+++ src/main/java/com/estafet/microservices/discovery/EstafetMicroservicesScrumDiscoveryClientApplication.java
@@ -0,0 +1,31 @@
+package com.estafet.microservices.discovery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@ComponentScan
+@RestController
+@SpringBootApplication
+public class EstafetMicroservicesScrumDiscoveryClientApplication {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@RequestMapping(name = "/project", method = RequestMethod.GET)
+	public String healthCheck() {
+		System.out.println(discoveryClient.getServices());
+		
+		return "";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EstafetMicroservicesScrumDiscoveryClientApplication.class, args);
+	}
+}
