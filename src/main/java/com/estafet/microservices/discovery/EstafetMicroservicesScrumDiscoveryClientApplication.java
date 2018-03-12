b44608d9444087c66574533fc66fe5c02ebc03d1
shukriev <shukri.izetov.shukriev@gmail.com>
2018-03-12 12:28:23 +0200

    init

--- src/main/java/com/estafet/microservices/discovery/EstafetMicroservicesScrumDiscoveryClientApplication.java
+++ src/main/java/com/estafet/microservices/discovery/EstafetMicroservicesScrumDiscoveryClientApplication.java
@@ -3,6 +3,7 @@ package com.estafet.microservices.discovery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan
 @RestController
 @SpringBootApplication
-public class EstafetMicroservicesScrumDiscoveryClientApplication {
+public class EstafetMicroservicesScrumDiscoveryClientApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
